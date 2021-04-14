ce8ea79d1380f6c9f58858482182b547d770fe03
Marcio de Jesus <marciodejesusrj@gmail.com>
2021-04-13 23:04:36 -0300

    listen env port

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+let port = process.env.PORT;
+
+if (port == null || port == '') {
+  port = 8000;
+}
 
 app.listen(port, ()=> {
   console.log(`Example app listening at http://localhost:${port}`)
