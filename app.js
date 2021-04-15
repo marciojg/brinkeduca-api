5b954e0ff3b86013b49c5ff1ba5021b65f295194
Marcio de Jesus <marciodejesusrj@gmail.com>
2021-04-14 21:09:47 -0300

    create a fake auth

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const students = [
+  { name: 'Sofia',    rm: 553  }, 
+  { name: 'Giovanna', rm: 251  }, 
+  { name: 'Estevan',  rm: 1894 }, 
+  { name: 'Marcos',   rm: 1632 }, 
+  { name: 'Livia',    rm: 544  }, 
+];
 
 const app = express();
 let port = process.env.PORT;
@@ -11,18 +18,20 @@ app.listen(port, ()=> {
   console.log(`Example app listening at PORT ${port}`)
 })
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('BrinkEduca')
 })
 
-app.get('/students', (req, res)=>{
-  res.send({
-    students: [
-      { name: 'Sofia',    rm: 553  }, 
-      { name: 'Giovanna', rm: 251  }, 
-      { name: 'Estevan',  rm: 1894 }, 
-      { name: 'Marcos',   rm: 1632 }, 
-      { name: 'Livia',    rm: 544  }, 
-    ],
-  })
+app.get('/students', (_, res)=>{
+  res.send({ students: students })
+})
+
+app.get('/students/auth/:rm', (req, res)=>{
+  const student = students.find(el => el.rm.toString() === req.params.rm);
+
+  if (student != null && student != undefined) {
+    res.send({ status: 'ok', data: student });
+  } else {
+    res.send({ status: 'error' });
+  }
 })
